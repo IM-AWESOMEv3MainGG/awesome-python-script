f5d23dd1ccbd943b56274050ca1108008fac5d4c
IM-AWESOMEv3MainGG <108638135+IM-AWESOMEv3MainGG@users.noreply.github.com>
2022-11-27 20:26:14 -0500

    Add files via upload

---
+++ Browser-Game-Backups/save_loader.py
@@ -0,0 +1,125 @@
+import sys
+import time
+import random
+import secrets
+import string
+import pyperclip
+import socket
+printing="Logged in! Welcome deecoyz to dee's save loader."
+def main():
+
+	print('Save Loader Key System (Enter key given from distributer)')
+	
+	password = 'key123'
+	user_input = input('Please Enter Purchase Key: ')
+	
+	if user_input != password:
+		sys.exit('Goodbye!')
+
+	print(printing)	
+
+if __name__ == "__main__":
+	main()
+print("Loading full program (dee's save loader)")
+time.sleep(0.2)
+print("Please enter save file desired (ifm, idlebreakout, calcevo, pixels-filling-squares, a-mazing-idle, idle-mine-remix, idle-roller, omega-layers, progress-knight, proto23, sword-fight, yet-another-merge-game")
+poo = input()
+fecal_matter = (poo)
+
+if fecal_matter == 'ifm':
+	f = open('assets//idle_football_save.txt', 'r')
+	file_contents = f.read()
+	print(file_contents)
+	f.close()
+	pyperclip.copy(file_contents)
+	time.sleep(0.1)
+	print('The save file has been copied to your clipboard.')
+if fecal_matter == 'idlebreakout':
+	f = open('assets//idle_breakout_save.txt', 'r')
+	file_contents = f.read()
+	print(file_contents)
+	f.close()
+	pyperclip.copy(file_contents)
+	time.sleep(0.1)
+	print('The save file has been copied to your clipboard.')
+if fecal_matter == 'calcevo':
+	f = open('assets//calc_evo_save.txt', 'r')
+	file_contents = f.read()
+	print(file_contents)
+	f.close()
+	pyperclip.copy(file_contents)
+	time.sleep(0.1)
+	print('The save file has been copied to your clipboard.')
+if fecal_matter == 'pixels-filling-squares':
+	f = open('assets//fill_squares.txt', 'r')
+	file_contents = f.read()
+	print(file_contents)
+	f.close()
+	pyperclip.copy(file_contents)
+	time.sleep(0.1)
+	print('The save file has been copied to your clipboard.')
+if fecal_matter == 'a-mazing-idle':
+	f = open('assets//a_mazing_idle_save.txt', 'r')
+	file_contents = f.read()
+	print(file_contents)
+	f.close()
+	pyperclip.copy(file_contents)
+	time.sleep(0.1)
+	print('The save file has been copied to your clipboard.')
+if fecal_matter == 'idle-mine-remix':
+	f = open('assets//idle_mine_remix.txt', 'r')
+	file_contents = f.read()
+	print(file_contents)
+	f.close()
+	pyperclip.copy(file_contents)
+	time.sleep(0.1)
+	print('The save file has been copied to your clipboard.')
+if fecal_matter == 'idle-roller':
+	f = open('assets//idle_roller.txt', 'r')
+	file_contents = f.read()
+	print(file_contents)
+	f.close()
+	pyperclip.copy(file_contents)
+	time.sleep(0.1)
+	print('The save file has been copied to your clipboard.')
+if fecal_matter == 'omega-layers':
+	f = open('assets//omega_layers.txt', 'r')
+	file_contents = f.read()
+	print(file_contents)
+	f.close()
+	pyperclip.copy(file_contents)
+	time.sleep(0.1)
+	print('The save file has been copied to your clipboard.')
+if fecal_matter == 'progress-knight':
+	f = open('assets//progress_knight.txt', 'r')
+	file_contents = f.read()
+	print(file_contents)
+	f.close()
+	pyperclip.copy(file_contents)
+	time.sleep(0.1)
+	print('The save file has been copied to your clipboard.')
+if fecal_matter == 'proto23':
+	f = open('assets//Proto23.txt', 'r')
+	file_contents = f.read()
+	print(file_contents)
+	f.close()
+	pyperclip.copy(file_contents)
+	time.sleep(0.1)
+	print('The save file has been copied to your clipboard.')
+if fecal_matter == 'sword-fight':
+	f = open('assets//sword_fight.txt', 'r')
+	file_contents = f.read()
+	print(file_contents)
+	f.close()
+	pyperclip.copy(file_contents)
+	time.sleep(0.1)
+	print('The save file has been copied to your clipboard.')
+if fecal_matter == 'yet-another-merge-game':
+	f = open('assets//yet_another_merge_game.txt', 'r')
+	file_contents = f.read()
+	print(file_contents)
+	f.close()
+	pyperclip.copy(file_contents)
+	time.sleep(0.1)
+	print('The save file has been copied to your clipboard.')
+
